ea2d054099e6a52a4a75d5fb07808839ad904142
Eduardo <ealvarez@carminaticonsulting.com>
2024-03-28 12:06:37 -0500

    vue options data example

--- vue3-project/src/components/TransactionList.vue
+++ vue3-project/src/components/TransactionList.vue
@@ -1,7 +1,15 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
       Gas Bill <span>$400</span>
       <button class="delete-btn">x</button>
     </li>
@@ -9,6 +17,20 @@
     <li class="plus">
       Paycheck <span>$400</span>
       <button class="delete-btn">x</button>
-    </li>
+    </li> -->
   </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: "Flower", amount: -19.99 },
+        { id: 2, text: "Salary", amount: 219.99 },
+        { id: 3, text: "Book Purchase", amount: -10.99 },
+      ],
+    };
+  },
+};
+</script>
